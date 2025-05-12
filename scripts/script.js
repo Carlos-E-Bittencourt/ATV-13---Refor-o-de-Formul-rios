bc2788e67c03b21eb3de77bceaa9e58d5954e490
Carlos-E-Bittencourt <carlos_bittencourt2@estudante.sesisenai.org.br>
2025-05-12 18:01:15 -0300

    UPDATE - Começo da declaração das variáveis

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,33 @@
+function enviar() {
+    const nome = document.getElementById('nome').value.trim();
+
+    const nascimento = document.getElementById('nascimento').value.trim();
+
+    const cpf = document.getElementById('cpf').value.trim();
+
+    const email = document.getElementById('email').value.trim();
+
+    const tel = document.getElementById('tel').value.trim();
+
+    const gender = document.querySelector('input[name="sex"]:checked');
+
+    const cidade = document.getElementById('cidade').value.trim();
+
+    const estado = document.getElementById('estado').value.trim();
+
+    const pais = document.getElementById('pais').value.trim();
+
+    const rua = document.getElementById('rua').value.trim();
+
+    const numeroCasa = document.getElementById('numeroCasa').value.trim();
+
+    const cep = document.getElementById('CEP').value.trim();
+
+    const color = document.querySelector('input[name="color"]:checked');
+
+
+}
+
 function passouOunao(){
     
 }
